b85f8f601c34d6aac1d5210b32b95c9c1030d8c3
skyloft <skyloftmb@gmail.com>
2026-04-28 00:55:59 -0400

    Set position method for convenience

--- src/engine/ecs/TransformComponent.java
+++ src/engine/ecs/TransformComponent.java
@@ -3,9 +3,10 @@ package engine.ecs;
 import org.joml.Matrix4f;
 
 @ComponentArray(mask = 1 << 7)
-public record TransformComponent(int transformIndex, Matrix4f transform) {
-    public TransformComponent(Matrix4f transform) {
-        this(0, transform);
+public record TransformComponent(Matrix4f transform) {
+    public void setPosition(float x, float y, float z) {
+        transform.m30(x);
+        transform.m31(y);
+        transform.m32(z);
     }
-
 }
