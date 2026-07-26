6e51c439ede59c35ae8e83227c54a48d7bbe3f2d
skyloft <skyloftmb@gmail.com>
2026-07-25 23:36:16 -0400

    Removed some useless classes

--- src/engine/SystemState.java
+++
@@ -1,6 +0,0 @@
-package engine;
-
-public class SystemState {
-    public static boolean running = true;
-
-}
--- src/engine/ecs/ScriptSystem.java
+++ src/engine/ecs/ScriptSystem.java
@@ -1,9 +1,5 @@
 package engine.ecs;
 
-import engine.SystemState;
-
-import java.util.List;
-
 public class ScriptSystem extends ActorSystem {
     private Scene scene;
 
@@ -13,20 +9,18 @@ public class ScriptSystem extends ActorSystem {
 
     @Override
     public void run(Actor root) {
-        if(SystemState.running) {
-            root.previsitAllActors(actor -> {
-                if (actor.has(ScriptComponent.class)) {
-                    ScriptComponent scriptComponent = actor.getComponent(ScriptComponent.class);
-                    if (!scriptComponent.script().initialized) {
-                        scriptComponent.script().init(actor);
-                        scriptComponent.script().initialized = true;
-                    }
-
-                    scriptComponent.script().update(actor, root);
-
+        root.previsitAllActors(actor -> {
+            if (actor.has(ScriptComponent.class)) {
+                ScriptComponent scriptComponent = actor.getComponent(ScriptComponent.class);
+                if (!scriptComponent.script().initialized) {
+                    scriptComponent.script().init(actor);
+                    scriptComponent.script().initialized = true;
                 }
-            });
-        }
+
+                scriptComponent.script().update(actor, root);
+
+            }
+        });
     }
 
     @Override
